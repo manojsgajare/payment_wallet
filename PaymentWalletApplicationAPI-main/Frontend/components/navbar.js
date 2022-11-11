c4d292fd45f376100890c8a4b1027f594169f5d1
HP <gajaremanoj1997@gmail.com>
2022-11-11 20:16:09 +0530

    project added

---
+++ PaymentWalletApplicationAPI-main/Frontend/components/navbar.js
@@ -0,0 +1,13 @@
+function navbar(){
+
+    return `
+    <div id = "signUp"><a href="index.html">SignUP</a></div>
+    <div id = "logIn"><a href="login.html">LogIn</a></div>
+    <div id = "Bank"><a href="addbank.html">Add Bank</a></div>
+    <div id = "beneficiary"><a href="beneficiary.html">Add beneficiary</a></div>
+    <div id = "makePayment"><a href="paybill.html">Pay Bill</a></div>
+    <div id = "transaction"><a href="transaction.html">Transaction</a></div>
+    `;
+}
+
+export {navbar}
